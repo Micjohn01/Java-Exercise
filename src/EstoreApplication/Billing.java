c6bd0bd432e9a33b30944e7f29c9802cf637e0b6
Micjohn01 <michaelolamilekanjohn0@gmail.com>
2023-03-04 14:11:54 +0100

    billing updated

---
+++ src/EstoreApplication/Billing.java
@@ -0,0 +1,8 @@
+package EstoreApplication;
+
+public class Billing {
+    private String receiversName;
+    private String phoneNumber;
+    private Address deliveryAddress;
+    private CreditCard creditCard;
+}

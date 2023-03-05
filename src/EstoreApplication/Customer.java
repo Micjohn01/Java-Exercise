c9db373897b6fd9758b53d5964ac73eaa2970b7e
Micjohn01 <michaelolamilekanjohn0@gmail.com>
2023-03-05 05:30:12 +0100

    customer updated

---
+++ src/EstoreApplication/Customer.java
@@ -0,0 +1,8 @@
+package EstoreApplication;
+
+import java.util.List;
+
+public class Customer extends User {
+    private List<Billing> billingInformation;
+    private ShoppingCart shoppingCart;
+}

9561a09d7199b86fe27a9d7a0b002ba14bc4de79
Micjohn01 <michaelolamilekanjohn0@gmail.com>
2023-03-05 05:28:31 +0100

    credit card updated

---
+++ src/EstoreApplication/CreditCard.java
@@ -0,0 +1,13 @@
+package EstoreApplication;
+
+import java.time.Month;
+import java.time.Year;
+
+public class CreditCard {
+    private String cvv;
+    private Year expirationYear;
+    private Month expirationMonth;
+    private String creditCardNumber;
+    private String nameOnCard;
+    private Card cardType;
+}
